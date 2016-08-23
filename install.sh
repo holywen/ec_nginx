5bc93696135bb9d7c61d4bd10aa0ce6ba5cf6274
Shaohua Wen <shaohua.wen@gmail.com>
2016-08-23 21:26:42 +0800

    add ask website

--- install.sh
+++ install.sh
@@ -5,7 +5,6 @@ cp ec.conf /www/wdlinux/nginx/conf/vhost
 mkdir -p  /www/web/cache
 mkdir -p /var/log/nginx
 mkdir -p /www/web/ec
+mkdir -p /www/web/askec
 
 cp -r  ec_static_content/* /www/web/ec
-
-
